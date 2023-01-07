d512449be8712495f5a8fa3abad8d1eb144a7003
Shane Delima <114932462+shane25p@users.noreply.github.com>
2023-01-07 20:41:50 +0000

    Create backup.py

---
+++ backup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+#
+# apt-get install python3-pip -y
+# pip install pydrive
+# pip install PyDrive2    <==== For Large files
+
+from pydrive2.drive import GoogleDrive
+from pydrive2.auth import GoogleAuth
+   
+# For using listdir()
+import os
+   
+  
+# Below code does the authentication
+# part of the code
+gauth = GoogleAuth()
+  
+# Creates local webserver and auto
+# handles authentication.
+gauth.LocalWebserverAuth()       
+drive = GoogleDrive(gauth)
+
+vc_directory = '<google drive directory id>'
+
+### Move Old files to Trash ###
+
+fileList = drive.ListFile({'q': "'<google drive directory id>' in parents and trashed=false"}).GetList()
+for file in fileList:
+  file_id = file['id']
+  gfile_in = drive.CreateFile({'id': file_id})
+  gfile_in.Trash()
+
+fileList = drive.ListFile({'q': "'<google drive directory id>' in parents and trashed=false"}).GetList()
+for file in fileList:
+  file_id = file['id']
+  gfile_in = drive.CreateFile({'id': file_id})
+  gfile_in.Trash()
+
+### vCenter Backup Upload to Google Drive ###
+
+local_vc_directory = "<directory of the file locally"
+for f in os.listdir(local_vc_directory):
+  filename = os.path.join(local_vc_directory, f)
+  gfile = drive.CreateFile({'parents' : [{'id' : vc_directory}], 'title' : f})
+  gfile.SetContentFile(filename)
+  gfile.Upload()
